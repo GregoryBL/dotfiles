d5794e4d90e217debb59fb9c5b9779322a1952aa
Greg Berns-Leone <gbernsleone@narrativescience.com>
2019-11-28 15:59:29 -0600

    Reorganize stuff

--- install.sh
+++ install.sh
@@ -2,7 +2,16 @@
 
 set -xe
 
-cp ./.zshrc             $HOME/.zshrc
-cp ./.gitignore_global  $HOME/.gitignore_global
+# Copy all homedirectory files into $HOME
+cp -r ./homedir/. $HOME
+
+# Set up some emacs directories
+mkdir -p $HOME/.emacs.d
+mkdir -p $HOME/.emacs.autosaves
+
+# Copy the shell load files
+cp -r ./loads $HOME/
+
+exec $SHELL
 
 set +xe
